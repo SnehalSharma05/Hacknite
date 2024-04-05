93b44117a6cfbed5ae4213dc1157391eed280dd6
SnehalSharma05 <158583829+SnehalSharma05@users.noreply.github.com>
2024-04-05 23:43:17 +0530

    user class

--- main.py
+++ main.py
@@ -41,3 +41,64 @@ Hufflepuff = house("Hufflepuff")
 Ravenclaw = house("Ravenclaw")
 Gryffindor = house("Gryffindor")
 Slytherin = house("Slytherin")
+
+class user():
+    users = []
+    names = []
+    ids = {}
+
+    def __init__(self, name, id, house=None, wand=None, points=0, wealth=0, potions=[], spells=['stupefy', 'expelliarmus', 'protego'], items=[], progress=0, enemiesDefeated=0, health=150):
+        self.name = name
+        self.id = id
+        self.house = house
+        self.health = health
+        self.max_health = health
+        self.enemiesDefeated = enemiesDefeated
+        self.wand = wand
+        self.spells = spells
+        self.points = points
+        self.level = self.points//30
+        self.wealth = wealth
+        self.potions = potions
+        self.items = items
+        self.progress = progress
+        user.users.append(self)
+        user.names.append(self.name)
+        user.ids[self.id] = self
+        self.revealed = False
+
+    def add_points(self, points):
+        self.points += points
+
+    def add_spell(self, spell):
+        self.spells.append(spell)
+
+    def add_potion(self, potion):
+        self.potions.append(potion)
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def set_house(self, house):
+        self.house = house
+
+    def get_info(self):
+        return self.name + " is in " + self.house + f" and has {self.points} points."
+
+    def get_full_info(self):
+        return self.name + " is in " + self.house + f" and has {self.points} points. You have the following spells: " + ",".join(self.spells) + "\nYou have the following wand: " + self.wand + "\nYour level is: " + str(self.level) + "\nYour max health is: " + str(self.max_health) + "\nNumber of enemies defeated: " + str(self.enemiesDefeated)
+
+    def get_spell_info(self):
+        return self.name + " has the following spells:\n" + "\n".join(self.spells)
+
+    def get_potion_info(self):
+        return self.name + " has the following potions:\n" + "\n".join(self.potions)
+
+    def get_item_info(self):
+        return self.name + " has the following items:\n" + "\n".join(self.items)
+
+    def get_house_info(self):
+        return self.name + " is in " + self.house + "."
+
+    def get_points_info(self):
+        return self.name + " has " + self.points + " points."
